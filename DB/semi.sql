da40121fcb0f36390c9a3f53b7575479f33e2127
Youngjin Mo <creator.youngjin@gmail.com>
2019-05-28 12:52:33 +0900

    add sql

---
+++ DB/semi.sql
@@ -0,0 +1,150 @@
+﻿create table member (
+    userid varchar2(50) primary key,    -- 유저 아이디. 이메일 형식
+    password varchar2(20) not null,     -- 비밀번호
+    nickname varchar2(30) not null,     -- 닉네임
+    iconno number not null,             -- 프로필 아이콘 번호 default 값 1
+    age number not null,                -- 연령대
+    gender varchar2(10) not null,       -- 성별 (남성,여성)
+    teamname varchar2(30) not null,     -- 좋아하는 팀 default 값 None(없음)
+    introduce varchar2(300),            -- 자기소개 입력. 
+    joindate date not null,             -- 가입날짜
+    grade number not null               -- 등급. 1 : 일반회원, 2 : 관리자. default 값 1
+);
+
+create sequence member_seq;
+
+select * from member;                   -- member 생성 확인
+
+create table icon (
+    iconno number primary key,          -- 아이콘 식별 번호. 자동생성
+    iconname varchar2(30) not null,     -- 아이콘 이름
+    filename varchar2(30) not null,     -- 아이콘 이미지 파일 명
+    storename varchar2(40) not null     -- 아이콘 이미지 저장 시 변형 이름
+);
+
+create sequence icon_seq;
+select * from icon;                     -- icon 생성 확인
+
+create table team (
+    teamname varchar2(30) primary key,  -- 팀 이름
+    iconno number not null,             -- 팀 아이콘 번호
+    region varchar2(15) not null,       -- 팀 연고지
+    stadium varchar2(30) not null,      -- 홈 경기장
+    event number not null               -- 경기 종목 1: 야구, 2: 축구
+);
+
+create sequence team_seq;
+select * from team;
+
+create table board (
+    boardno number primary key,         -- 게시판 번호 (자동생성)
+    userid varchar2(50) not null,       -- 작성자 아이디
+    title varchar2 (150) not null,      -- 게시판 제목
+    content varchar2 (3000),            -- 게시판 본문
+    scheduleno number not null,         -- 선택한 직관 스케줄 번호
+    insertdate date not null,           -- 게시판 작성일
+    hit number not null                 -- 게시판 조회수
+);
+
+create sequence board_seq;
+select * from board;
+
+create table report (
+    reportno number primary key,        -- 신고글 번호 (자동생성)
+    userid varchar2 (50) not null,      -- 신고당한 유저 아이디
+    reason varchar2 (150) not null,     -- 신고 사유
+    explain varchar2 (1500),            -- 신고 사유 설명
+    reportdate date not null            -- 신고글 작성 날짜
+);
+
+create sequence report_seq;
+select * from report;
+
+create table getout (
+    getoutid varchar2 (50) primary key, -- 제재회원 아이디. 재가입 불가
+    reason varchar2 (150) not null,     -- 제재 사유 선택
+    explain varchar2 (1500)             -- 제재 사유 설명
+);
+
+create sequence getout_seq;
+select * from getout;
+
+create table reply (
+    replyno number primary key,         -- 댓글 번호
+    userid varchar2 (50) not null,      -- 댓글 작성자 아이디
+    boardno number not null,            -- 댓글 작성한 게시글 번호
+    recontent varchar2 (900) not null,  -- 댓글 내용
+    insertdate date not null            -- 댓글 작성일
+);
+
+create sequence reply_seq;
+select * from reply;
+
+create table schedule (
+    scheduleno number primary key,      -- 스케줄 번호
+    hometeam varchar2 (30) not null,    -- 홈팀 이름
+    awayteam varchar2 (30) not null,    -- 원정팀 이름
+    gamedate date not null              -- 경기 날짜
+);
+
+create sequence schedule_seq;
+select * from schedule;
+
+create table exit (
+    exitid varchar2 (50) primary key,   -- 탈퇴 회원 아이디
+    exitreason varchar2 (1500)          -- 탈퇴 사유
+);
+
+create sequence exit_seq;
+select * from exit;
+
+alter table member                      -- member table iconno 외래키 지정
+add constraint iconno_fk
+    foreign key(iconno)
+    references icon(iconno);
+
+alter table member                      -- member table iconno 외래키 지정
+add constraint teamname_fk
+    foreign key(teamname)
+    references team(teamname);
+
+alter table team
+add constraint teamicon_fk
+    foreign key (iconno)
+    references icon(iconno);
+
+alter table board
+add constraint boardid_fk
+    foreign key (userid)
+    references member (userid);
+    
+alter table board
+add constraint scheduleno_fk
+    foreign key (scheduleno)
+    references schedule (scheduleno);
+
+alter table report
+add constraint reportid_fk
+    foreign key (userid)
+    references member (userid);
+
+alter table reply
+add constraint replyid_fk
+    foreign key (userid)
+    references member (userid);
+
+alter table reply
+add constraint boardno_fk
+    foreign key (boardno)
+    references board (boardno);
+
+alter table schedule
+add constraint hometeam_fk
+    foreign key (hometeam)
+    references team(teamname);
+
+alter table schedule
+add constraint awayteam_fk
+    foreign key (awayteam)
+    references team(teamname);
+commit;
--- README.md
+++ README.md
@@ -1,17 +1,122 @@
-# Semi Project
+# Semi-Project - 1조
 
-### Project Name
-
-아직 조 이름이 정해지지 않은 관계로 프로젝트 폴더 이름은 temp로 두었으며, 향후 조 이름이 정해지는대로 수정하겠습니다.
+2019년 4월 23일에 작성한 1주차 회의록.
 
 <br>
 
-<h3>Subject</h3>
+![](http://newsimg.hankookilbo.com/2019/03/09/201903091713088016_22.jpg)
 
-스포츠 경기와 관련한 데이터를 가져와서 가공하여 보여주는 웹 어플리케이션이 목적입니다.
+
+
+# What we did
+
+오늘 있었던 회의록 통해 확정된 부분을 정리했습니다.
+
+주제를 분명하게 설정했고, 웹 어플리케이션에 추가할 기능과 웹 페이지를 간략하게 정리했습니다.
 
 <br>
 
-### Contributors
+### 목차
 
-모든 팀원들이 해당 레파지토리의 contributor로 등록되면 이후 기재합니다.
+- [주제](#subject)
+- [웹 페이지](#web-pages)
+- [기능](#features)
+  - [User DB](#user-db)
+  - [Search Event](#search-event)
+  - [Calenar](#calendar)
+  - [Other](#other)
+  - [API](#api)
+
+<br>
+
+## Subject
+
+### old. 축구경기 DB 조회 플랫폼
+
+야구경기처럼 축구 경기에서 발생하는 데이터를 가져와서 가공해서 보여주는 데이터 플랫폼을 초기에 기획했으나 기획과정에서 어떤 데이터를, 어떻게 가져올지에 대해 구상하던 중 사실상 실현 불가능하다는 점을 파악하였습니다. 우리가 원했던 데이터를 import 할 수 있는 API가 시중에 존재하지 않았기 때문입니다.
+
+bottom-up 방식으로 축구경기를 통해 우리가 가져올 수 있는 데이터가 무엇인지부터 다시 조사하였으며 이를 통해 주제를 변경했음을 알려드립니다. 
+
+변경된 주제는 아래와 같습니다. 
+
+<br>
+
+### new. 직관 매칭 플랫폼
+
+지역[서울, 부산,.. ]과 종목[축구, 야구]을 입력받아서 스포츠 경기 일정을 보여주고, 게시판에서 같이 경기장에 응원 갈 사람을 연결시켜주는 웹 어플리케이션.
+
+<br>
+
+## Web Pages
+
+- Main Page
+  - Login (pop up)
+  - Signup
+- About - company
+- My page - user
+  - view information
+    - update
+    - delete account
+- Calendar page
+- Matching page
+- bottom page
+  - terms of use
+  - privacy
+
+
+
+## Features
+
+### User DB
+
+- 회원 가입
+  - 아이디
+    - 아이디 중복 확인
+  - 비밀번호
+    - 비밀번호 확인
+  - 이름
+  - 주민등록번호 뒷자리 하나까지
+- 회원 정보
+  - 로그인
+  - 로그아웃
+  - 정보 수정
+  - 아이디/비밀번호 찾기
+- 회원 탈퇴
+
+
+
+### Search Event (on Main)<a name="search-event"></a>
+
+- \* **종목** (축구/야구)
+- \* **지역**
+- 팀 (optional)
+
+
+
+### Calendar (linked from Main)<a name="calendar"></a>
+
+- 경기 정보
+- 날씨 정보 (API, optional)
+- 미세먼지 정보 (API, optional)
+
+
+
+## Other
+
+- 1:1 채팅 (Direct Message)
+  오프라인에서 만날 수 있도록 연락처 공유 기능이 **반드시 필요**하나 어떻게 구축할 수 있을지 모름.
+
+
+
+## API
+
+- Calendar API
+- Direct Message API (like Telegram)
+
+
+
+# What we will do
+
+- 스토리보드를 일정보다 빨리 진행하여 주제 및 계획 선정에서 어려운 추상적인 부분을 제거할 필요가 있다. 스토리보드를 만들면서 상세한 페이지와 기능 기획이 필요하다.
+- UI 디자이너가 없기 때문에 우리 주제와 가장 부합하는 주제의 레퍼런스 어플리케이션 확보가 중요. 각자 레퍼런스로 삼을 웹 어플리케이션 발견시 Slack에 공유.
+
